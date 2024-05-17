4fd3f0c9366596d24575522684dae990d6cd06c9
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2024-05-17 11:24:08 -0300

    Feat: Add Linter and Formatter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,60 @@
 module.exports = {
+  // Stops ESLint from searching more config files to merge in ancestor
+  // directories.
   root: true,
-  env: { browser: true, es2020: true },
+  // Target environment.
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  // Provides sets of rules which I might individually apply and may also
+  // provides zero or more configuration files.
+  plugins: ["@typescript-eslint", "react", "react-hooks", "react-refresh"],
+  // Apply configuration files sequentially. They may be sourced from a npm
+  // package or exported from a plugin. Each groups several rules together that
+  // are logically bounded.
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    // Should always be last to disable any formatting rules set by previous
+    // configuration files.
+    "prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  // Shared settings for all configuration files.
+  settings: {
+    // Required by eslint-plugin-react.
+    react: {
+      version: "detect",
+    },
+  },
+  // Allows ESLint to parse TypeScript code.
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
+  // Override configuration for this specific file.
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.cjs"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
